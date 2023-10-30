74ebc0955923e101f78a456e2fb72d4ab7fbcc28
Alexandre Maquet <alexandre.maquet+1@he-arc.ch>
2023-10-30 14:21:53 +0100

    Update uses

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Http\Controllers\ListController;
 use Illuminate\Support\Facades\Route;
 
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ListController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\UserController;
 
