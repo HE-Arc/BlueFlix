74ce547463f009e449eebc2c7e9b56d29dcbd716
Alexandre Maquet <alexandre.maquet+1@he-arc.ch>
2023-10-30 14:22:19 +0100

    Add home view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends("layout.app")
+
+@section("content")
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="page-header">
+				<h1>
+					Blueflix
+				</h1>
+			</div>
+			<div class="jumbotron">
+				<h2 class="display-4">Welcome!</h2>
+				<p class="lead">Blueflix is a website where you can create lists of movies and series you want to watch.</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+@endsection
