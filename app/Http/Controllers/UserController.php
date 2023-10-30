eab0816d7a29e17f8fed824814bfea80848550dc
Moreira Dos Santos Daniel <daniel.moreiradossantos@he-arc.ch>
2023-10-30 11:36:14 +0100

    Let user login

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        if(Auth::check()) {
+            $user = Auth::user();
+        }
+
+
+        return view('users.login');
+    }
+
+    public function parseLogin(Request $request)
+    {
+        $request->validate([
+            'username' => 'required|string|max:255',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::where('username', $request->input('username'))->first();
+
+        if (Auth::attempt(['username' => $request->input('username'), 'password' => $request->input('password')])) {
+            return redirect()->route('login')->with('success', 'You are logged in.');
+            #return redirect()->route('welcome')->with('success', 'You are logged in.');
+        } else {
+            return redirect()->route('login')->with('error', 'Username or password is incorrect.');
+        }
+    }
+}
