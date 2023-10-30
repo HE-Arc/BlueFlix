8511b1c16a20c3ec1d747543d7896c1e7fa1139a
Alexandre Maquet <alexandre.maquet+1@he-arc.ch>
2023-10-30 14:39:52 +0100

    Update navbar

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -24,12 +24,15 @@
                     </li>
                     @auth
                         <li class="nav-item">
-                            <a class = "nav-link" href="{{ url('/logout') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Logout</a>
-                        </li>
-                        <li class="nav-item">
                             <a class = "nav-link" href="{{ url('/profil') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Profil</a>
                         </li>
+                        <li class="nav-item">
+                            <a class = "nav-link" href="{{ url('/logout') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Logout</a>
+                        </li>
                         @else
+                        <li class="nav-item">
+                            <a class = "nav-link" href="{{ url('/register') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                        </li>
                         <li class="nav-item">
                             <a class = "nav-link" href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Login</a>
                         </li>
