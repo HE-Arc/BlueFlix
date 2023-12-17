2df75da0c1284eb27e0242e95085ff2235529811
Alexandre Maquet <alexandre.maquet+1@he-arc.ch>
2023-12-17 14:28:11 +0100

    Update to add data needed for ajax to work

--- app/Http/Controllers/SerieController.php
+++ app/Http/Controllers/SerieController.php
@@ -2,15 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\Api;
+use App\Models\Liste;
+use App\Models\SerieListe;
 
 class SerieController extends Controller
 {
     public function showSerie($id)
     {
         $apiData = Api::getSerieDetails($id);
-
-        return view('series.details', ['serie' => $apiData]);
+        if(Auth::check()) {
+            $query = Liste::where('user_id', auth()->user()->id);
+            $lists = $query->select('id', 'nom')->get();
+            $checkedLists = SerieListe::whereIn('liste_id', $query->select('id'))->where('serie_id', $id)->select('liste_id')->get();
+            $checkedLists = $checkedLists->pluck('liste_id')->toArray();
+        } else {
+            $lists = [];
+            $checkedLists = [];
+        }
+        return view('series.details', ['serie' => $apiData, 'lists' => $lists, 'checkedLists' => $checkedLists, 'elementId' => $apiData->id, 'elementType' => 'serie']);
     }
 }
