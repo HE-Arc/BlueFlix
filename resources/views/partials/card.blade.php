0cd705c00629dfd75f0608910e44b565ee04bffd
Oguey Stéphane <stephane.oguey@he-arc.ch>
2023-12-17 20:16:29 +0100

    Add default image

--- resources/views/partials/card.blade.php
+++ resources/views/partials/card.blade.php
@@ -1,7 +1,13 @@
 <div class="col-md-3 mb-3">
     <a href="{{ $cardInfo->route }}">
         <div class="card clickable-card" style="width: 18rem;">
-            <img src="{{$cardInfo->image}}" class="card-img-top" alt="Image Placeholder">
+            @if (!empty($cardInfo->image))
+                <img src="{{$cardInfo->image}}" class="card-img-top" alt="Card Image">
+            @else
+                <!-- Image de remplacement -->
+                <img src="{{ asset('images/default/list.png') }}" class="card-img-top" alt="Image de Remplacement">
+            @endif
+
             <div class="card-body">
                 <h5 class="card-title">{{$cardInfo->title}}</h5>
                 <div class="additional-buttons">
