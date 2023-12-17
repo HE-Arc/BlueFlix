2663c200b7cc49a7db5a169d633957c3471ab07b
Alexandre Maquet <alexandre.maquet+1@he-arc.ch>
2023-12-17 14:28:11 +0100

    Update to add data needed for ajax to work

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -2,15 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\Api;
+use App\Models\Liste;
+use App\Models\FilmListe;
 
 class FilmController extends Controller
 {
     public function showFilm($id)
     {
         $apiData = Api::getFilmDetails($id);
-
-        return view('films.details', ['film' => $apiData]);
+        if(Auth::check()) {
+            $query = Liste::where('user_id', auth()->user()->id);
+            $lists = $query->select('id', 'nom')->get();
+            $checkedLists = FilmListe::whereIn('liste_id', $query->select('id'))->where('film_id', $id)->select('liste_id')->get();
+            $checkedLists = $checkedLists->pluck('liste_id')->toArray();
+        } else {
+            $lists = [];
+            $checkedLists = [];
+        }
+        return view('films.details', ['film' => $apiData, 'lists' => $lists, 'checkedLists' => $checkedLists, 'elementId' => $apiData->id, 'elementType' => 'film']);
     }
 }
