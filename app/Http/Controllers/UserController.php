bfe8c1f8ce15609ca70380f89974f08d0ed348db
Moreira Dos Santos Daniel <daniel.moreiradossantos@he-arc.ch>
2023-12-18 19:04:50 +0100

    Add comment to user controller.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,18 +14,27 @@ use App\Models\CardInfo;
 
 class UserController extends Controller
 {
+    /**
+     * Display the user profile and associated lists.
+     *
+     * @param int $id User ID
+     * @return \Illuminate\View\View
+     */
     public function index($id)
     {
+        // Find user by ID
         $user = User::find($id);
 
+         // Redirect to home if user not found
         if($user == null) {
             return redirect()->route('home')->with('error', 'User not found !');
         }
 
+        // Get user's lists
         $lists = Liste::where('user_id', $id)->get();
 
+        // Transform lists into CardInfo objects for display
         $results = [];
-        //title, image, route("route('lists.show', $list->id)")
         foreach ($lists as $list) {
             $newelement = new CardInfo();
             $newelement->title = $list->nom;
@@ -36,46 +45,76 @@ class UserController extends Controller
             array_push($results, $newelement);
         }
 
+        // Render user profile view with associated lists
         return view('users.profil', compact('user'),['results' => $results]);
     }
 
+    /**
+     * Display the login form.
+     *
+     * @return \Illuminate\View\View
+     */
     public function login()
     {
         return view('users.login');
     }
 
 
+    /**
+     * Display the register form.
+     *
+     * @return \Illuminate\View\View
+     */
     public function register()
     {
         return view('users.register');
     }
 
+    /**
+     * Logout the authenticated user.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function logout()
     {
         Auth::logout();
         return redirect()->back()->with('success', 'You are logged out.');
     }
 
+    /**
+     * Validate and process user login.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function parseLogin(Request $request)
     {
+        // Validate user input
         $request->validate([
             'username' => 'required|string|max:255',
             'password' => 'required|string|min:8',
         ]);
 
-        #$user = User::where('username', $request->input('username'))->first();
-
+        // Attempt to log the user in
         if (Auth::attempt(['username' => $request->input('username'), 'password' => $request->input('password')])) {
+            // Redirect to user profile if login successful
             return redirect()->route('profil', ['id' => auth()->id()])->with('success', 'You are logged in.');
-            #return redirect()->action([UserController::class, 'index'], ['id' => auth()->id()])->with('success', 'You are logged in.');
 
         } else {
+            // Redirect to login page with error message if login failed
             return redirect()->route('login')->withErrors(['loginError' => 'Username or password is incorrect.'])->withInput();
         }
     }
 
+    /**
+     * Validate and process user registration.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function createUser(Request $request)
     {
+        // Validate user input
         $request->validate([
             'firstname' => 'required|string|max:255',
             'lastname' => 'required|string|max:255',
@@ -107,11 +146,19 @@ class UserController extends Controller
 
         $this->createDefaultList($user);
 
+        // Log the user in
         auth()->login($user);
 
+        // Redirect to user profile
         return redirect()->route('profil', ['id' => auth()->id()])->with('success', 'Account created successfully!');
     }
 
+    /**
+     * Create default lists for a new user.
+     *
+     * @param \App\Models\User $user
+     * @return void
+     */
     private function createDefaultList(User $user)
     {
         $li = new Liste();
@@ -142,25 +189,44 @@ class UserController extends Controller
         Liste::create($ToSee);
     }
 
+    /**
+     * Display the edit form.
+     *
+     * @param int $id User ID
+     * @return \Illuminate\View\View
+     */
     public function edit($id)
     {
+        // Find user by ID
         $user = User::find($id);
 
+        // Redirect to home if user not found
         if (auth()->user()->id !== $user->id) {
             return redirect()->route('home')->with('error', 'You don\'t have access to this ressource !');
         }
 
+        // Render user edit view
         return view('users.edit', compact('user'));
     }
 
+    /**
+     * Validate and process user update.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id User ID
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function update(Request $request, $id)
     {
+        // Find user by ID
         $user = User::find($id);
 
+        // Redirect to home if user not found
         if (auth()->user()->id !== $user->id) {
             return redirect()->route('home')->with('error', 'You don\'t have access to this ressource !');
         }
 
+        // Validate user input
         $request->validate([
             'firstname' => 'required|string|max:255',
             'lastname' => 'required|string|max:255',
