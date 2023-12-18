f85be622582b674d4e3434bdf121fab244dd5d6d
Oguey Stéphane <stephane.oguey@he-arc.ch>
2023-12-18 14:28:08 +0100

    Add rating system

---
+++ app/Models/RatingSerie.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RatingSerie extends Model
+{
+    use HasFactory;
+
+    public $incrementing = false;
+
+    protected $fillable = ['serie_id', 'user_id', 'rating'];
+
+    protected $primaryKey = ['serie_id', 'user_id'];
+
+    public function film()
+    {
+        return $this->belongsTo(Serie::class, 'serie_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
