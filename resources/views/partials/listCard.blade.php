f181df1077323b1dad26b3209a18513388540c51
Alexandre Maquet <alexandre.maquet+1@he-arc.ch>
2023-12-18 22:07:24 +0100

    Remove old text

--- resources/views/partials/listCard.blade.php
+++ resources/views/partials/listCard.blade.php
@@ -2,7 +2,7 @@
     <a href="{{ $cardInfo->route }}">
         <div class="card clickable-card">
             @if (!empty($cardInfo->image))
-                <img src="{{$cardInfo->image}}" class="card-img-top" alt="Image Placeholder">
+                <img src="{{$cardInfo->image}}" class="card-img-top" alt="Image">
             @else
                 <img src="{{ asset('images/default/list.png') }}" class="card-img-top" alt="Image Placeholder">
             @endif
