b981f08a5d10dcbcbeebce69eff163b4518f7f8b
Moreira Dos Santos Daniel <daniel.moreiradossantos@he-arc.ch>
2023-12-18 18:22:46 +0100

    Put the profil image at the left og the profil's information.

--- resources/views/users/profil.blade.php
+++ resources/views/users/profil.blade.php
@@ -5,15 +5,18 @@
         <h2 class="alignedTitle">Profil</h2>
         <div class="container">
             <div class="profilContainer p-4 mb-4" style="background-color: #f8f9fa;">
-                <div>
-                    <img src="{{asset("images/$user->urlImage")}}" alt="Image Placeholder">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="{{ asset("images/$user->urlImage") }}" alt="Image Placeholder" class="img-fluid">
+                    </div>
+                    <div class="col-md-8">
+                        <p>Username: {{ $user->username }}</p>
+                        <p>Email: {{ $user->email }}</p>
+                        <p>Firstname: {{ $user->firstname }}</p>
+                        <p>Lastname: {{ $user->lastname }}</p>
+                        <p>Created at: {{ $user->created_at }}</p>
+                    </div>
                 </div>
-                <p>Username: {{ $user->username }}</p>
-                <p>Email: {{ $user->email }}</p>
-                <p>Firstname: {{ $user->firstname }}</p>
-                <p>Lastname: {{ $user->lastname }}</p>
-                <p>Created at: {{ $user->created_at }}</p>
-                <div></div>
             </div>
 
         </div>
@@ -23,14 +26,14 @@
         @endif
 
         @isset($results)
-        <div class="card-container row">
-            @foreach ($results as $result)
-                <div class="col-md-4 mb-4">
-                    @include("partials.listCard", ["cardInfo" => $result])
-                </div>
-            @endforeach
-        </div>
-    @endisset
+            <div class="card-container row">
+                @foreach ($results as $result)
+                    <div class="col-md-4 mb-4">
+                        @include("partials.listCard", ["cardInfo" => $result])
+                    </div>
+                @endforeach
+            </div>
+        @endisset
 
     </main>
 @endsection
